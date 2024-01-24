2868593f3f5884b89fc758f6e61e1ee1456f046a
ST359 <135271126+ST359@users.noreply.github.com>
2024-01-24 04:11:34 +0300

    feat: subtask create.js complete

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import fs from "node:fs/promises";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+  const srcFolder = `${__dirname}\\files`;
+  const destinationFolder = `${__dirname}\\files_copy`;
+  const fsError = "FS operation failed";
+  await fs.access(srcFolder, fs.constants.R_OK).catch(() => {
+    throw new Error(fsError);
+  });
+  await fs
+    .cp(srcFolder, destinationFolder, {
+      errorOnExist: true,
+      recursive: true,
+      force: false,
+    })
+    .catch(() => {
+      throw new Error(fsError);
+    });
 };
 
 await copy();
