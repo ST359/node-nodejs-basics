65a6ae14948de50da396a152d870a47410b05f02
ST359 <135271126+ST359@users.noreply.github.com>
2024-01-24 05:46:05 +0300

    feat: subtask rename.js complete

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { error } from "node:console";
+import fs from "node:fs/promises";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+  const src = `${__dirname}\\files\\wrongFilename.txt`;
+  const destination = `${__dirname}\\files\\properFilename.md`;
+  const fsError = "FS operation failed";
+  await fs.access(src).catch(() => {
+    throw new Error(fsError);
+  });
+  await fs.access(destination)
+    .then(() => {
+      throw new Error(fsError);
+    })
+    .catch((error) => {
+      if (error.message === fsError) {
+        throw error;
+      }
+      fs.rename(src, destination).catch((error)=>console.error(error));
+    });
 };
 
 await rename();
