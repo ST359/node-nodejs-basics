be0c819b61ac150104f49ff51517c0275d653943
ST359 <135271126+ST359@users.noreply.github.com>
2024-01-25 07:42:02 +0300

    feat: subtask env.js complete

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+
+
+
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = process.env;
+    let RSSVars = [];
+    for (let key in envVars){
+        if (key.startsWith('RSS_')){
+            RSSVars.push(`${key}=${envVars[key]}`);
+        }
+    }
+    const parsed = RSSVars.join('; ');
+    console.log(parsed);
 };
 
 parseEnv();
