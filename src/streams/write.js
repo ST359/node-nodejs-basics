03f143445fd2ff5643966635345789c4e9e1127e
ST359 <135271126+ST359@users.noreply.github.com>
2024-01-26 05:24:54 +0300

    feat: subtask streams write.js complete

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from "node:fs";
+import process from "node:process";
+import { dirname } from "path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const fileToWrite = `${__dirname}\\files\\fileToWrite.txt`;
+    const writeableStream = createWriteStream(fileToWrite);
+    process.stdin.pipe(writeableStream);
 };
 
 await write();
